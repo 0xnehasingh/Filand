48a9993f9246e092085cf450247ca47b6d5e37e1
0xnehasingh.eth <67492324+0xnehasingh@users.noreply.github.com>
2023-07-28 12:34:57 +0530

    added timestamp conversion

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+import loanABI from "./Loan.json";
+import tokenABI from "./Token.json";
+export const loanAbi = loanABI.abi;
+export const tokenAbi = tokenABI.abi;
+export const loanContractAddress = "0x76B8d8dE1dD5AdC7ec137518248013DCB168Dd08";
+export const tokenContractAddress =
+    "0xd72430ca142f5EA97c816d547bD4096411f0486c";
